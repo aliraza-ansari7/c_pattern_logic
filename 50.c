f52111bb67a6297554601b1a14269c08e5365ebe
aliraza-ansari7 <aliraza.ansari8188@gmail.com>
2026-08-13 14:37:29 +0530

    pattern example 50

---
+++ 50.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int i,j;
+
+for(i=1;i<=5;i++)
+{	
+char ch='A';
+	for(j=1;j<=9;j++)
+	{ 
+	 
+		if(j>=10-i || j<=i )
+		   printf("%c",ch);
+
+		else
+		   printf(" ");	
+ch++;
+	
+}
+
+printf("\n");
+}
+
+return 0;
+}
