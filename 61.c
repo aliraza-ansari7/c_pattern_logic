a3843002a2e081b29a3acc0f78110514e0665c75
aliraza-ansari7 <aliraza.ansari8188@gmail.com>
2026-08-15 23:23:44 +0530

    pattern example 61

---
+++ 61.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int i,j;
+
+for(i=1;i<=11;i++)
+{	
+char ch='A';
+	for(j=1;j<=11;j++)
+	{ 
+	 
+		if(j<=i || j<=12-i)
+		   printf("%c",ch);
+
+		else
+		   printf(" ");
+ch++;	
+}
+
+printf("\n");
+}
+return 0;
+}
