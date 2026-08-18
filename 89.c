c7e886fef310f3c81ae93032d0b055633208859a
aliraza-ansari7 <aliraza.ansari8188@gmail.com>
2026-08-18 20:14:43 +0530

    pattern example 89

---
+++ 89.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int i,j;
+
+for(i=1;i<=9;i++)
+{	
+char ch='A';
+	for(j=1;j<=9;j++)
+	{ 
+		if(j==i|| i==1 || i==9 || j==1 || j==9) 
+		   printf("%c",ch);
+
+		else
+		   printf(" ");	
+	ch++;
+	}
+
+
+printf("\n");
+}
+
+return 0;
+}
+
