61b26dbfc429ed0260994a9f47647de6b81919f9
aliraza-ansari7 <aliraza.ansari8188@gmail.com>
2026-08-21 23:12:24 +0530

    pattern example 109

---
+++ 109.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+int i,j;
+
+for(i=0;i<=10;i++)
+{	
+int num=1;
+
+	for(j=0;j<=13;j++)
+	{ 
+		if(j<=2+i || j>=1-i)
+			{
+				if(i==0 && j==0)
+			 	printf(" ");
+			} 
+		   printf("* ");
+
+			
+		
+		if(j<=2+i && j>=i )
+		{
+			printf("%d",num);
+			num=num+1; 
+                        
+		}
+		
+	}
+
+
+printf("\n ");
+}
+
+return 0;
+}
