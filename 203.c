ecfa6f905cd6e74031bdee8ed3494a0ae8cf34dd
aliraza-ansari7 <aliraza.ansari8188@gmail.com>
2026-09-04 22:40:46 +0530

    pattern example 203

---
+++ 203.c
@@ -0,0 +1,22 @@
+#include<Stdio.h>
+int main()
+{
+int i,j;
+
+for(i=0;i<=10;i++)
+{
+char ch='A';
+	for(j=0;j<=10;j++)
+	{
+	if(j<=10-i || j<=i)
+		printf("%c",ch);
+
+		else
+		printf(" ");
+	
+	}
+printf("\n");
+}
+
+return 0;
+}
